b0cb041bbe4f9865a2fe84574f38803e3797f6a0
ZakariaElkhadir <zelkhadir5@gmail.com>
2025-01-01 15:26:51 +0100

    Implement user registration endpoint with password hashing

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-import express from 'express'
-import bcrypt from 'bcrypt'
+import express from "express";
+import bcrypt from "bcrypt";
 
-const app = express()
+const app = express();
 const port = 5000;
+app.use(express.json());
 
 // In-memory
 const users = [];
 
-app.use(express.json());
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // Find user
+    const findUser = users.find((data) => email == data.email);
+    if (findUser) {
+      res.status(400).send("wrong email or password !");
+    }
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // push email and password into users array
+    users.push({ email, password: hashedPassword });
+    res.status(201).send("Account created successfuly!");
+    return;
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
 
 app.listen(port, () => {
-    console.log('Server is started on', port)
-})
+  console.log("Server is started on", port);
+});
